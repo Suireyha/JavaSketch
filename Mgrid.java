368eef8d6dced4551b46bc3d1f03bd55896944b1
Suireyha <Marvink2296@gmail.com>
2025-08-18 21:45:25 +1000

    Cleaned up the code and added a bunch of comments, sideBar is now justified properly

--- Mgrid.java
+++ Mgrid.java
@@ -4,36 +4,66 @@ import javax.swing.*;
 
 public class Mgrid{
     public static void main(String[] args) throws Exception{
-        int pxCount = 0;
-        Color inFocus = new Color(52, 53, 54);
+        //General initialisation and declaration
+        int pxCount = 0; //Used for giving each pixel and ID
+        Color inFocus = new Color(52, 53, 54); //Colours for when the panel is in/out of focus
         Color noFocus = new Color(47, 48, 49);
-        GridBagConstraints flex = new GridBagConstraints();
-        pixel[][] grid = new pixel[20][20];
-        int length = grid.length;
+        GridBagConstraints justifyPanels = new GridBagConstraints(); //Used for justifying the layout of the master panels
+        pixel[][] grid = new pixel[20][20]; //Create a 20x20 array of pixel objects (setup happens later)
+        int length = grid.length; //Used for loops during setup of the canvas
 
+        //The master frame
+        JFrame win = new JFrame("MGrid");
 
-        JPanel panel = new JPanel(new GridBagLayout());
-        panel.setSize(500, 500);
-        //panel.add(new btn(50, 50), new GridBagConstraints());
+        //Creating instances for the different panels
+        JPanel global = new JPanel(new GridBagLayout()); //Master panel which everything is in
+        
+        JPanel sideBar = new JPanel(); //Contains side bar elements
+        JPanel canvas = new JPanel(new GridLayout(20,20)); //Contains the pixels
 
+        //Setup for the master frame
+        win.setBounds(200, 200, 900, 900); //Place a 900x900 window at 200,200 (200 px down and right from top left)
+        win.setContentPane(global); //global is the main parent pannel
+        //Setup for the panels
+        sideBar.setPreferredSize(new Dimension(50,500));
+        sideBar.setBackground(new Color(255,0,0)); //Red (Just for now so it's visible, will be invisible once we add elements to it, sorta like a div)
+
+        canvas.setPreferredSize(new Dimension(500, 500));
+
+        global.setSize(900, 900);
+        global.setOpaque(true);
+        global.setFocusable(true);
+        global.setBackground(inFocus);
+  
+
+        justifyPanels.gridx = 0;
+        justifyPanels.weightx = 1.0; //This line is essential!! It makes the canvas margin fill all, pushing the sideBar far right as long as it's got a weight of 0
+        justifyPanels.anchor = GridBagConstraints.CENTER; //Center justification
+        global.add(canvas, justifyPanels); //Global is the parent of canvas
+        
+        justifyPanels.gridx = 1;
+        justifyPanels.weightx = 0;
+        justifyPanels.anchor = GridBagConstraints.EAST;
+        global.add(sideBar, justifyPanels); //Global is the parent of sideBar
+
+        //Create the grid of pixels
         for(int x = 0; x < length; x++){
-            flex.gridy++;
             for(int y = 0; y < length; y++){
+                //Actually create the new pixel and add it
                 grid[x][y] = new pixel(25, 25, pxCount);
-                panel.add(grid[x][y], flex); //Note, you HAVE to use the GridBagConstraints type here otherwise it overloads to something else >:()
-                pxCount++;
+                canvas.add(grid[x][y]); //Note, you HAVE to use the GridBagConstraints type here otherwise it overloads to something else >:()
+                pxCount++; //Used for the IDs, increment with every loop
             }
-            
         }
 
-        panel.setOpaque(true);
-        panel.setFocusable(true);
+        System.out.println(pxCount); //Prints the number of pixels
+        System.out.println(length); //Prints the width/length of the grid
+        win.setVisible(true); //This little piece of shit MUST be called after everything is added, I can't believe I just wasted 2 hours bug fixing THIS.
 
-        JFrame win = new JFrame("MGrid");
-        win.setBounds(200, 200, 800, 800); //Place a 800x800 window at 200,200 (200 px down and right from top left)
-        win.setContentPane(panel);
-        System.out.println("Hello world");
-        win.setVisible(true);
+
+
+        
+        //Event listeners
 
         //Close the window when user closes the window :fire:
         //frame.addWindowListener is very similar to add event listener in js
@@ -44,25 +74,21 @@ public class Mgrid{
             }
         });
 
-        panel.addFocusListener(new FocusAdapter(){
+        global.addFocusListener(new FocusAdapter(){
             @Override
             public void focusLost(FocusEvent e){
                 System.out.println("Lost focus... :(");
-                win.getContentPane().setBackground(noFocus);
-                panel.setBackground(noFocus);
+                global.setBackground(noFocus);
             }
         });
 
-        panel.addFocusListener(new FocusAdapter(){
+        global.addFocusListener(new FocusAdapter(){
             @Override
             public void focusGained(FocusEvent e){
                 System.out.println("Welcome back!! :)");
-                win.getContentPane().setBackground(inFocus);
-                panel.setBackground(inFocus);
+                global.setBackground(inFocus);
             }
         });
-
-        
         
     }
 }
